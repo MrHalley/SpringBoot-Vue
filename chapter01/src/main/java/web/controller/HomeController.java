2919fcde41082d5dea7fbb7a47cf0058ef287dba
Mr.Du <1165449791@qq.com>
2020-09-22 18:42:12 +0800

    书本源码

---
+++ chapter01/src/main/java/web/controller/HomeController.java
@@ -0,0 +1,12 @@
+package web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello spring boot!";
+    }
+}
