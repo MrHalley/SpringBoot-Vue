8f9149bd6e3feb9680c7840c74de5f6d4318ef7b
Mr.Du <1165449791@qq.com>
2020-09-30 15:30:35 +0800

    除去某个自动配置

--- chapter4/aop/src/main/java/org/sang/AopApplication.java
+++ chapter4/aop/src/main/java/org/sang/AopApplication.java
@@ -1,9 +1,12 @@
 package org.sang;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class AopApplication {
 
     public static void main(String[] args) {
